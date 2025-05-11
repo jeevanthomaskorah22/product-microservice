a49e2f3f42a55101170a932e9830a0a9106b19a6
eldho1markose <157269287+eldho1markose@users.noreply.github.com>
2025-05-11 19:57:27 +0530

    Update product.py

--- app/routers/product.py
+++ app/routers/product.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter,Depends,status,HTTPException
 from .. import schemas,models
 from sqlalchemy.orm import Session
 from ..database import get_db
+from ..token import verify_token
 
 router= APIRouter(
     prefix='/products',
@@ -12,12 +13,12 @@ router= APIRouter(
 #CRUD 
 
 @router.get('/',response_model=List[schemas.ShowProduct])
-def all(db:Session =Depends(get_db)):
+def all(db:Session =Depends(get_db), token: dict = Depends(verify_token)):
     products=db.query(models.Product).all()
     return products
 
 @router.post('/',status_code=status.HTTP_201_CREATED)
-def create(request :schemas.Product , db : Session =Depends(get_db) ):
+def create(request :schemas.Product , db : Session =Depends(get_db),token: dict = Depends(verify_token) ):
     new_product= models.Product(name=request.name,price=request.price,stock=request.stock)
     db.add(new_product)
     db.commit()
@@ -25,7 +26,7 @@ def create(request :schemas.Product , db : Session =Depends(get_db) ):
     return new_product
 
 @router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id,db:Session=Depends(get_db)):
+def destroy(id,db:Session=Depends(get_db),token: dict = Depends(verify_token)):
     product =db.query(models.Product).filter(models.Product.id==id)
     if not product.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'product with id {id} is not available')
@@ -35,7 +36,7 @@ def destroy(id,db:Session=Depends(get_db)):
 
 #update
 @router.put('/{id}',status_code=status.HTTP_202_ACCEPTED)
-def update(id,request: schemas.Product,db:Session=Depends(get_db)):
+def update(id,request: schemas.Product,db:Session=Depends(get_db),token: dict = Depends(verify_token)):
     product=db.query(models.Product).filter(models.Product.id==id)
     if not product.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'product with id {id} is not available')
@@ -45,7 +46,7 @@ def update(id,request: schemas.Product,db:Session=Depends(get_db)):
     return 'updated'
 
 @router.get('/{id}',status_code=200,response_model=schemas.ShowProduct)
-def show(id,db:Session =Depends(get_db)):
+def show(id,db:Session =Depends(get_db),token: dict = Depends(verify_token)):
     product=db.query(models.Product).filter(models.Product.id==id).first()
     #responce is used for exception handling
     if not product:
